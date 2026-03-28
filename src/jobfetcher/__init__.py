5e5ebb6b9bd83241778d698ea23a1fb69cd53fa9
wuyagithub <kechundong@gmail.com>
2026-03-28 14:51:43 +0800

    cleanup: remove redundant files
    Deleted:
    - src/ (old unused architecture)
    - jobs.json (redundant export)
    - cookie_manager.py (opencli doesn't need cookies)
    - hooks/ (auto-push hook removed)
    
    Updated .gitignore to exclude generated outputs.

--- src/jobfetcher/__init__.py
+++
@@ -1,3 +0,0 @@
-# JobFetcher - Job Search Aggregation Agent
-
-__version__ = "0.1.0"
--- src/jobfetcher/scrapers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Job scrapers for various platforms."""
-
-from jobfetcher.scrapers.base import BaseScraper
-from jobfetcher.scrapers.linkedin import LinkedInScraper
-
-__all__ = [
-    "BaseScraper",
-    "LinkedInScraper",
-]
